aef88702f2b502fdcc3730995ff45e1b053631f3
Ernandes Maia <ernandes.maiamq@gmail.com>
2020-02-03 06:38:53 -0300

    CMT 2: Consultas ao Mongo (quase) funcionando

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
 const app = express();
 
 /*const charts = require('./routs/charts');*/
@@ -25,3 +28,48 @@ const porta = 3000;
 
 //Iniciando o servidor
 app.listen(porta, () => console.log('Servidor iniciado na porta ${porta}'));
+
+//Consulta
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("test");
+    
+    dbo.collection("PauDosFerrosSIM").find({"dia_semana_obito": "dom"}).toArray(function(err, result) {
+        if (err) throw err;
+            console.log(result.length);
+            db.close();
+    });
+    
+    dbo.collection("PauDosFerrosSIM").find({"dia_semana_obito": "seg"}).toArray(function(err, result) {
+        if (err) throw err;
+            console.log(result.length);
+            db.close();
+    });
+    
+    dbo.collection("PauDosFerrosSIM").find({"dia_semana_obito": "ter"}).toArray(function(err, result) {
+        if (err) throw err;
+            console.log(result.length);
+            db.close();
+    });
+
+    dbo.collection("PauDosFerrosSIM").find({"dia_semana_obito": "qua"}).toArray(function(err, result) {
+        if (err) throw err;
+            console.log(result.length);
+            db.close();
+    });
+    
+    /*dbo.collection("PauDosFerrosSIM").find({"dia_semana_obito": "qui"}).toArray(function(err, result) {
+        if (err) throw err;
+            console.log(result.length);
+            vetor[4] = result.length;
+            db.close();
+    });
+
+    dbo.collection("PauDosFerrosSIM").find({"dia_semana_obito": "sex"}).toArray(function(err, result) {
+        if (err) throw err;
+            console.log(result.length);
+            vetor[5] = result.length;
+            db.close();
+    });*/
+
+});
--- public/main.js
+++ public/main.js
@@ -1,7 +1,4 @@
 //noprotect
 document.addEventListener("DOMContentLoaded", function(){
-    fetch('http://localhost/3000', {
-        method: '',
-    }).then().catch(err => console.log(err));
 
 });
